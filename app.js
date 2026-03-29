d23f692a9c5e0b199cb0a2e979c86baade6b0edf
lavanyayadav8888-blip <lavanyayadav.8888@gmail.com>
2026-03-29 21:04:02 +0000

    Load users

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 /* add your code here */
+const userList = document.querySelector(".user-list");
 
+function loadUsers() {
+  userList.innerHTML = "";
+
+  users.forEach(user => {
+    const li = document.createElement("li");
+    li.textContent = `${user.firstname} ${user.lastname}`;
+    
+    li.addEventListener("click", () => selectUser(user));
+    
+    userList.appendChild(li);
+  });
+}
